e515ea0e09132ed531dfc36342dbc93c148796c6
Alex Koutmos <akoutmos@gmail.com>
2020-04-10 12:00:39 -0400

    Checkpoint

--- lib/unplug.ex
+++ lib/unplug.ex
@@ -1,18 +1,48 @@
 defmodule Unplug do
   @moduledoc """
-  Documentation for `Unplug`.
+  This plug acts as a filter for Plug.Telemetry and allows you to filter out
+  events based on the incoming URL.
+
+  When servicing requests, Phoenix will log information about the incoming
+  request and outgoing response. In some cases it may pollute the logs
+  if certain endpoints are triggered and logged at regular intervals (for
+  example logging for `/metrics` when using something like Prometheus to
+  collect metrics or `/health` if you are health checking your application
+  with Kubernetes).
+
+  To use this in your application, open up your `endpoint.ex` file and replace:
+  `plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]`
+
+  with
+
+  `plug TelemetryFilter, filter_endpoints: ["/metrics", "/health"], event_prefix: [:phoenix, :endpoint]`
+
+  Feel free to replace the values for `:filter_endpoints` with whatever endpoints
+  you would like to omit telemetry events for.
   """
 
-  @doc """
-  Hello world.
+  @behaviour Plug
 
-  ## Examples
+  plug(Unplug, run_plug: Plug.Telemetry, run_plug_opts: [], eval: {Unplug}, eval_opts: [])
 
-      iex> Unplug.hello()
-      :world
+  @impl true
+  def init(opts) do
+    plug_telemetry_opts = Telemetry.init(opts)
+    telemetry_filter_opts = Keyword.get(opts, :filter_endpoints, [])
 
-  """
-  def hello do
-    :world
+    {plug_telemetry_opts, telemetry_filter_opts}
+  end
+
+  @impl true
+  def call(conn, {plug_telemetry_opts, []}) do
+    Telemetry.call(conn, plug_telemetry_opts)
+  end
+
+  def call(conn, {plug_telemetry_opts, telemetry_filter_opts}) do
+    if Enum.member?(telemetry_filter_opts, conn.request_path) do
+      conn
+    else
+      Telemetry.call(conn, plug_telemetry_opts)
+    end
   end
 end
--- mix.exs
+++ mix.exs
@@ -5,8 +5,17 @@ defmodule Unplug.MixProject do
     [
       app: :unplug,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
+      name: "Unplug",
+      source_url: "https://github.com/akoutmos/unplug",
+      homepage_url: "https://hex.pm/packages/unplug",
+      description: "A plug that can be used to conditionally invoke other plugs",
       start_permanent: Mix.env() == :prod,
+      docs: [
+        main: "readme",
+        extras: ["README.md"]
+      ],
+      package: package(),
       deps: deps()
     ]
   end
@@ -18,11 +27,20 @@ defmodule Unplug.MixProject do
     ]
   end
 
+  defp package() do
+    [
+      name: "unplug",
+      files: ~w(lib mix.exs README.md LICENSE CHANGELOG.md),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/akoutmos/unplug"}
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:plug, "~> 1.8"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
