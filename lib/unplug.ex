81dd1de924e72b2f5bc947df006ec17d3337bbb9
Alex Koutmos <akoutmos@gmail.com>
2020-04-10 11:49:20 -0400

    Initial commit

---
+++ lib/unplug.ex
@@ -0,0 +1,18 @@
+defmodule Unplug do
+  @moduledoc """
+  Documentation for `Unplug`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Unplug.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/unplug_test.exs
@@ -0,0 +1,8 @@
+defmodule UnplugTest do
+  use ExUnit.Case
+  doctest Unplug
+
+  test "greets the world" do
+    assert Unplug.hello() == :world
+  end
+end
