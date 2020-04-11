e3e3e710061dfa7ea510414975f0b1b1c7a7168b
Alex Koutmos <akoutmos@gmail.com>
2020-04-11 16:35:30 -0400

    Adding missing travis entry in mix.exs

--- mix.exs
+++ mix.exs
@@ -18,7 +18,13 @@ defmodule Unplug.MixProject do
       package: package(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.travis": :test
+      ]
     ]
   end
 
