19a730355ef7a8c92d5d9c09a336d557dfe0f05a
Anderson Cook <andersonmcook@gmail.com>
2020-04-11 11:13:20 -0500

    add anonymous functions for if/do/else

--- test/unplug_test.exs
+++ test/unplug_test.exs
@@ -1,8 +1,29 @@
 defmodule UnplugTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+  use Plug.Test
   doctest Unplug
 
-  test "greets the world" do
-    assert Unplug.hello() == :world
+  test "accepts anonymous functions for if/do/else" do
+    opts =
+      Unplug.init(
+        if: fn c -> c.assigns[:thing] == "nice" end,
+        do: fn c -> assign(c, :cool, "it's good") end,
+        else: fn c -> assign(c, :no, "it's bad") end
+      )
+
+    conn =
+      :get
+      |> conn("/")
+      |> Unplug.call(opts)
+
+    assert conn.assigns == %{no: "it's bad"}
+
+    conn =
+      :get
+      |> conn("/")
+      |> assign(:thing, "nice")
+      |> Unplug.call(opts)
+
+    assert conn.assigns == %{cool: "it's good", thing: "nice"}
   end
 end
